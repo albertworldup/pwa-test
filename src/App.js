3caf9f6b34b81faa3c4484b7c2f052486c572d1d
admin <admin@admin.com>
2021-01-18 12:52:53 +0100

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import * as History from 'history';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Transfer from "./Containers/Transfer";
+
 const history = History.createBrowserHistory();
 
 function App() {
@@ -17,8 +19,8 @@ function App() {
         <Route exact path="/back">
           <>Back</>
         </Route>
-        <Route exact path="/test">
-          <>Test</>
+        <Route exact path="/transfer">
+          <Transfer />
         </Route>
       </Switch>
     </Router>
---
+++ src/Containers/Transfer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Transfer() {
+    return (
+        <div>
+            <h1>Great</h1>
+        </div>
+    );
+}
+
+export default Transfer;
